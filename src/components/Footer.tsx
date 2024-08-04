16a476a67c673b33fd8ff3c1970f8db7054bd663
briskaputraa <briskaananda103@gmail.com>
2024-08-04 13:04:57 +0700

    Mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,32 +1,32 @@
 const Footer = () => {
   return (
-    <div className="w-full h-fit flex z-11 absolute justify-center bottom-0 bg-cover bg-center my-10  border-t-2 border-slate-300">
-      <div className="w-[90vw] h-fit z-10 ">
-        <div className="flex flex-row align-center justify-between py-10">
-          <div>
-            <h1 className="text-5xl font-semibold text-slate-900 mb-2">
+    <div className="w-full h-fit flex absolute justify-center bottom-0 bg-cover bg-center my-10 border-t-2 border-slate-300">
+      <div className="w-[90vw] h-fit z-10">
+        <div className="flex flex-col sm:flex-row align-center justify-between py-5 md:py-10">
+          <div className="mb-6 sm:mb-0">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-900 mb-1">
               Wanna Work Together
             </h1>
-            <p className="text-2xl text-slate-500">
+            <p className="text-lg sm:text-xl md:text-2xl text-slate-500">
               Take a shoot to connect with me!
             </p>
           </div>
 
-          <div className="h-fill flex justify-center items-center">
-            <button className="inline-flex px-4 py-2 animate-shimmer items-center justify-center h-fit rounded-full font-semibold text-white border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <div className="h-fill flex justify-center items-center w-full sm:w-auto">
+            <button className="w-full sm:w-auto inline-flex px-4 py-2 animate-shimmer items-center justify-center h-fit rounded-full font-semibold text-white border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
               Take a Shoot
             </button>
           </div>
         </div>
 
-        <div className="w-full flex flex-row justify-between">
-          <div className="text-sm font-normal text-slate-500">
+        <div className="w-full flex flex-col sm:flex-row justify-between">
+          <div className="text-sm font-normal text-slate-500 mb-4 sm:mb-0">
             <p>
               Made with NextJs and designed happily using Figma by Briska Ananda
             </p>
           </div>
 
-          <div className="flex flex-row gap-x-5 justify-end">
+          <div className="flex flex-row gap-x-5 justify-center sm:justify-end">
             <a href="" className="text-sm font-normal text-slate-500">
               Github
             </a>
